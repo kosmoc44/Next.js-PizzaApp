10b4cb1a4f20c2499408ad742405e3dbed4369ad
Akramjanov Asadbke <kosmoc44@gmail.com>
2025-06-13 00:09:35 +0500

    delete top sw

--- components/shared/topBart.tsx
+++ components/shared/topBart.tsx
@@ -12,7 +12,7 @@ export const TopBar: React.FC<Props> = ({ className }) => {
   return (
     <div
       className={cn(
-        "sticky top-0 bg-white/50 py-2 shadow-sm z-10",
+        "sticky top-0 bg-white/50 py-2 shadow-[0_2px_4px_-1px_rgba(0,0,0,0.1)] z-10",
         className
       )}
     >
